0364a73ffa01f4a47a9255ec5455918acd659914
Jithu SJ <jithusj169@gmail.com>
2025-09-24 16:03:17 +0530

    made the gpt search

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import GptSearchBar from './GptSearchBar'
+import GptMovieSuggestion from './GptMovieSuggestion'
 
 const GptSearch = () => {
   return (
     <div>
-      
+      <GptSearchBar />
+      <GptMovieSuggestion />
     </div>
   )
 }
